d133db7f3fc349efd9f42b99485257f83c2c9293
ming <songming@biomap.com>
2025-11-23 03:31:48 +0800

    feat: add figure plotting scripts & relevant data

---
+++ r_utils.r
@@ -0,0 +1,32 @@
+# color map
+zeileis_28 <- c(
+  "#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00",
+  "#FFFF33", "#A65628", "#F781BF", "#999999", "#66C2A5",
+  "#FC8D62", "#8DA0CB", "#E78AC3", "#A6D854", "#FFD92F",
+  "#E5C494", "#B3B3B3", "#8DD3C7", "#FFFFB3", "#BEBADA",
+  "#FB8072", "#80B1D3", "#FDB462", "#B3DE69", "#FCCDE5",
+  "#D9D9D9", "#BC80BD", "#CCEBC5"
+)
+
+major_group_col <- c(
+    'Case' = "#3B4992",
+    'Ctrl' = '#008B45'
+)
+
+age_group_col <- c(
+    '[20, 40)' = "#CCEBC5",
+    '[40, 60)' = "#4DAF4A",
+    '[60, 80)' = "#80B1D3",
+    '[80, 100)' = "#1782ae",
+    '>=100' = "#464a96"
+)
+
+gender_col <- c(
+    'F' = "#514fef",
+    'M' = "#FB8072"
+)
+
+change_col <- c(
+    'up' = "#E41A1C",
+    'down' = "#514fef"
+)
